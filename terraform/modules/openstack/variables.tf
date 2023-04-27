b8a003fbfaf2a75031a418ebc65dd9cbb444116a
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2023-04-27 16:44:15 -0400

    First Working ansible config with proxy + galaxy on one machine and the database on another

--- terraform/modules/openstack/variables.tf
+++ terraform/modules/openstack/variables.tf
@@ -4,8 +4,8 @@ variable "instances_info" {
         volume_size = optional(number, 30)
         image = optional(string, "Ubuntu-22.04.2-Jammy-x64-2023-02")
         image_uuid = optional(string, "db73980e-1f9c-441e-8268-c1881f99c8ef")
-        network_uuid = optional(string, "39e2642a-7afe-4e64-abf2-5c6a7b05912d")
-        security_groups = optional(list(string), ["web", "TUSd", "default"])
+        network_uuid = optional(string, "94db2a0a-14a4-4934-896d-a28bbc651b09")
+        security_groups = optional(list(string), ["default"])
     }))
 }
 
