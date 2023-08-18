158c9bf6f0bfbe778ea4439cbd5a4734ac837574
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2023-08-18 16:49:54 -0400

    pass provider to volume attach

--- pulumi/openstack/volume.ts
+++ pulumi/openstack/volume.ts
@@ -3,40 +3,45 @@ import { VolumeConfig } from './schema';
 import { Provider } from './provider';
 
 export class Volume extends blockstorage.Volume {
-    attachment: VolumeAttach;
-    constructor(
-        config: VolumeConfig,
-        instance: compute.Instance,
-        provider: Provider
-    ) {
-        super(
-            config.name,
-            {
-                volumeType: config.type,
-                size: config.size,
-            },
-            { provider: provider }
-        );
-        this.attachment = new VolumeAttach(config.name, this, instance);
-    }
+  attachment: VolumeAttach;
+  constructor(
+    config: VolumeConfig,
+    instance: compute.Instance,
+    provider: Provider
+  ) {
+    super(
+      config.name,
+      {
+        volumeType: config.type,
+        size: config.size,
+      },
+      { provider: provider }
+    );
+    this.attachment = new VolumeAttach(config.name, this, instance, provider);
+  }
 }
 
 export class VolumeAttach extends compute.VolumeAttach {
-    constructor(
-        name: string,
-        volume: blockstorage.Volume,
-        instance: compute.Instance
-    ) {
-        super(name, {
-            volumeId: volume.id,
-            instanceId: instance.id,
-        });
-    }
+  constructor(
+    name: string,
+    volume: blockstorage.Volume,
+    instance: compute.Instance,
+    provider: Provider
+  ) {
+    super(
+      name,
+      {
+        volumeId: volume.id,
+        instanceId: instance.id,
+      },
+      { provider: provider }
+    );
+  }
 }
 
 export const VOLUME_TYPE = {
-    SSD: 'volumes-ssd',
-    HDD: 'volumes-ec',
+  SSD: 'volumes-ssd',
+  HDD: 'volumes-ec',
 } as const;
 
 export type VolumeType = keyof typeof VOLUME_TYPE;
