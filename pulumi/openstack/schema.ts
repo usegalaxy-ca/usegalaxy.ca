056df743efea1f470694071593b05ceb5cf82856
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2023-08-18 13:16:28 -0400

    fix schema

--- pulumi/openstack/schema.ts
+++ pulumi/openstack/schema.ts
@@ -12,48 +12,51 @@ const flavors = Object.keys(FLAVOR_CPU) as Flavor[];
 const FLAVORS: [Flavor, ...Flavor[]] = [flavors[0] as Flavor, ...flavors];
 const flavor = z.enum(FLAVORS);
 
-const instanceSchema = z
+const instanceSchema = z.record(
+  identifier,
+  z
     .object({
-        name: identifier,
-        flavor: flavor,
-        volume_size: size.optional(),
-        image: identifier.optional(),
-        image_uuid: uuid.optional(),
-        networks_uuid: uuid.optional(),
-        security_groups: identifier.array().optional(),
-        count: count.optional(),
+      name: identifier,
+      flavor: flavor,
+      volume_size: size.optional(),
+      image: identifier.optional(),
+      image_uuid: uuid.optional(),
+      networks_uuid: uuid.optional(),
+      security_groups: identifier.array().optional(),
+      count: count.optional(),
     })
-    .strict();
+    .strict()
+);
 
 export type InstanceConfig = z.infer<typeof instanceSchema>;
 
 const volumeSchema = z
-    .object({
-        name: identifier,
-        type: identifier,
-        size: size,
-        attach_to: identifier,
-    })
-    .strict();
+  .object({
+    name: identifier,
+    type: identifier,
+    size: size,
+    attach_to: identifier,
+  })
+  .strict();
 
 const floatingIpSchema = z
-    .object({
-        ip: ip,
-        attach_to: identifier,
-    })
-    .strict();
+  .object({
+    ip: ip,
+    attach_to: identifier,
+  })
+  .strict();
 
 const clusterSchema = z.record(
-    identifier,
-    z.object({
-        instances: z.array(instanceSchema),
-        volumes: z.array(volumeSchema),
-        floating_ips: z.array(floatingIpSchema),
-    })
+  identifier,
+  z.object({
+    instances: z.array(instanceSchema),
+    volumes: z.array(volumeSchema),
+    floating_ips: z.array(floatingIpSchema),
+  })
 );
 
 export type ClusterConfig = z.infer<typeof clusterSchema>;
 
 export function validate(yaml: string): ClusterConfig {
-    return clusterSchema.parse(yaml);
+  return clusterSchema.parse(yaml);
 }
