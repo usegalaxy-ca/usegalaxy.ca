2ac420ae22ecf805633c7da613742f0b1b9b49c7
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2023-09-18 18:08:33 -0400

    variable domain name for tool script

--- ansible/roles/usegalaxy_ca.tools/files/install_tools.sh
+++ ansible/roles/usegalaxy_ca.tools/files/install_tools.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 set -e
 
-API_KEY=$1
+DOMAIN=$1
+API_KEY=$2
 
 source .venv/bin/activate
-shed-tools install -g https://jl-galaxy.usegalaxy.ca -a $API_KEY -t files/galaxy/tool_list.yml
+shed-tools install -g https://$DOMAIN -a $API_KEY -t files/galaxy/tool_list.yml
